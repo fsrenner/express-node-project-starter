14400942abf2b461f996920c4cd7115c003efed5
Steven Renner <1521881760@mil>
2020-09-04 10:43:19 -0400

    created better mvc setup

---
+++ config/config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const bunyan = require('bunyan');
+
+module.exports = {
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 3000,
+    logger: bunyan.createLogger({
+        name: 'expressApplication',                     // Required
+        level: 'debug',      // Optional, see "Levels" section
+        // stream: <node.js stream>,           // Optional, see "Streams" section
+        // streams: [<bunyan streams>, ...],   // Optional, see "Streams" section
+        // serializers: <serializers mapping>, // Optional, see "Serializers" section
+        // src: <boolean>,                     // Optional, see "src" section
+     
+        // Any other fields are added to all log records as is.
+        // foo: 'bar',
+    }),
+    multer: {
+        storage: multer.memoryStorage()
+    }
+};
--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 // Node modules requires
 require('dotenv').config();
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const helmet = require('helmet');
 const cors = require('cors');
 const compression = require('compression');
-const multer = require('multer');
-const errorHandler = require('errorhandler');
-const flatten = require('flat');
+
+// run this command in the terminal
+// DEBUG=http:server node app.js
 const debug = require('debug');
 const debugLog = debug('http:server');
 
 // local file requires
-const Logger = require('./logger');
+const config = require(`${__dirname}/config/config`);
+const router = require('./router');
 
 // Global static variables
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const HOST = config.host;
+const PORT = config.port;
 
 // Global functions
 const app = express();
-const router = express.Router();
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const Logger = config.logger;
 
 // Express middleware
+app.use('/', router);
 app.use(helmet());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(compression());
 app.use(session({
@@ -42,34 +38,12 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
     app.set('trust proxy', 1)
     sess.cookie.secure = true
   }
-app.use('/', (req, res, next) => {
-    Logger.debug(`${req.protocol} ${req.method} request made from ${req.hostname} and ip ${req.ip} to path ${req.path} using original url ${req.originalUrl} with content-type ${req.get('content-type')}`);
-    next();
-});
-
-const errorLogHandler = (err, str) => { Logger.error(`The following error occurred: ${err}, details: ${str}`) };
-if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
-    app.use(errorHandler({ log: errorLogHandler }));
-}
-
-
-app.get('/status', (req, res) => {
-    const body = req.body;
-    const query = req.query
-    return res.json({
-        status: `The application is up and running on ${HOST}:${PORT}. Data sent:`,
-        body,
-        query
-    });
-});
-
 
 app.listen(PORT, HOST, (err) => {
     if (err) {
         throw new Error(err);
-    } else {
-        Logger.info(`Server listening at ${HOST}:${PORT}`)
     }
+    Logger.info(`Server listening at ${HOST}:${PORT}`)
 })
 
 
