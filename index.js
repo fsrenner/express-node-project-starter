abce4a56469b9662c13e9c0f286024c76221894e
Steven Renner <1521881760@mil>
2020-09-04 08:39:05 -0400

    Continued adding middleware and setting up a couple of basic status type routes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require('dotenv').config();
-
 // Node modules requires
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const helmet = require('helmet');
-const compression = require('compression')
+const cors = require('cors');
+const compression = require('compression');
 const multer = require('multer');
+const errorHandler = require('errorhandler');
 const flatten = require('flat');
 const debug = require('debug');
 const debugLog = debug('http:server');
@@ -19,15 +22,46 @@ const PORT = process.env.PORT || 3000;
 // Global functions
 const app = express();
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // Express middleware
 app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(compression());
+app.use(session({
+    secret: 'session secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1)
+    sess.cookie.secure = true
+  }
+app.use('/', (req, res, next) => {
+    Logger.debug(`${req.protocol} ${req.method} request made from ${req.hostname} and ip ${req.ip} to path ${req.path} using original url ${req.originalUrl} with content-type ${req.get('content-type')}`);
+    next();
+});
+
+const errorLogHandler = (err, str) => { Logger.error(`The following error occurred: ${err}, details: ${str}`) };
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+    app.use(errorHandler({ log: errorLogHandler }));
+}
 
 
-
-
-
-
+app.get('/status', (req, res) => {
+    const body = req.body;
+    const query = req.query
+    return res.json({
+        status: `The application is up and running on ${HOST}:${PORT}. Data sent:`,
+        body,
+        query
+    });
+});
 
 
 app.listen(PORT, HOST, (err) => {
